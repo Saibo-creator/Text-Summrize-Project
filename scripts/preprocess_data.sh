b88918f7823c3056b6ccf6a7c2cd841f14098ce5
root <root@iccluster033.iccluster.epfl.ch>
2020-03-08 16:57:36 +0100

    feat sync

--- scripts/preprocess_data.sh
+++ scripts/preprocess_data.sh
@@ -8,4 +8,5 @@ cd ..
 #mv datasets/yelp_dataset/yelp_academic_dataset_review.json datasets/yelp_dataset/review.json  (out of date)
 # PYTHONPATH=. python data_loaders/yelp_dataset.py
 
-PYTHONPATH=. python3 data_loaders/hotel_dataset.py
+#PYTHONPATH=. python3 data_loaders/hotel_dataset.py
+PYTHONPATH=. python3 data_loaders/hotel_mask_dataset.py
