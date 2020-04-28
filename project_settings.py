9a2af0d7b4c63cb9a56d122cd608e6df1e6c5f90
Saibo-creator <saibo.geng@yahoo.fr>
2020-04-28 10:45:47 +0800

    ready to train sing asp sum

--- project_settings.py
+++ project_settings.py
@@ -143,7 +143,7 @@ class DatasetConfig(object):
             # Trained models
             #self.lm_path = 'stable_checkpoints/lm/mlstm/hotel/batch_size_64-lm_lr_0.001/lm_e49_intermediate.pt' not working
             #self.lm_path = 'checkpoints/lm/mlstm/hotel_mask/lm_e9_2.93.pt' not working
-            self.lm_path ='checkpoints/lm/mlstm/hotel_mask_sing_asp/lm_e24_2.88yelp.pt'
+            self.lm_path ='checkpoints/lm/mlstm/hotel_mask_sing_asp/default_8d1b9abf/lm_e14_2.99pt'
             self.clf_path = 'checkpoints/clf/cnn/hotel_mask_sing_asp/batch_size_64-clf_lr_0.0005-cnn_n_feat_maps_256/clf_e2_l0.8130_a0.6326_d0.0000.pt'
             self.sum_path = 'checkpoints/sum/train/hotel_mask_sing_asp/batch_size_8-notes_./sum_e2_sub1_2.33_r1f0.31.pt'
             self.autoenc_path = None
