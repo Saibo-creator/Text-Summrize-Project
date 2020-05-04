1e39337da690a7d3207cc940af048a3341da2588
Saibo-creator <saibo.geng@yahoo.fr>
2020-05-04 14:44:34 +0800

    regular

--- gene_sub_encoder.sh
+++ gene_sub_encoder.sh
@@ -5,10 +5,18 @@
 for f in hotel_mask_asp_0 hotel_mask_asp_1 hotel_mask_asp_2 hotel_mask_asp_3 hotel_mask_asp_4
 do
 
+mkdir -p "datasets/${f}_dataset/processed/subwordenc/"
+
+mkdir -p "checkpoints/lm/mlstm/${f}"
+mkdir -p "checkpoints/clf/cnn/${f}"
+mkdir -p "checkpoints/sum/train/${f}"
+mkdir -p "checkpoints/sum/mlstm/${f}"
+mkdir -p "outputs/eval/${f}"
+
 bash scripts/preprocess_data.sh "${f}_dataset.py"
 
-PYTHONPATH=.  python3 data_loaders/build_subword_encoder.py --dataset=${f} \
---output_dir="./datasets/${f}_dataset/processed/subwordenc/" --output_fn=subwordenc
+# PYTHONPATH=.  python3 data_loaders/build_subword_encoder.py --dataset=${f} \
+# --output_dir="./datasets/${f}_dataset/processed/subwordenc/" --output_fn=subwordenc
 
 
 done
