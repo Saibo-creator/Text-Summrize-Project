4f77daed7c65bf8c44a7f1bd407088b43b8fa4aa
Saibo-creator <saibo.geng@yahoo.fr>
2020-05-13 05:17:20 +0200

    len loss move to cuda done

--- models/summarization.py
+++ models/summarization.py
@@ -242,7 +242,8 @@ class SummarizationModel(nn.Module):
             # length_loss = length_cos(ideal_length,
             #                          summ_texts_lengths )#input_txts_mean_lengths  # 0<=loss<=1( not strictly <1 but summ_text should merely exceed twice the input length in practice)
 
-            self.stats['length_loss'] = move_to_cuda(torch.mean(torch.ones(extra['shortness'].shape)-move_to_cuda(extra['shortness'])))#10*[6.4844e-01, 4.2873e-06, 8.9063e-01, 7.0317e-02, 5.4688e-01, 7.1737e-06],
+            self.stats['length_loss'] = move_to_cuda(torch.mean(move_to_cuda(torch.ones(extra['shortness'].shape))-move_to_cuda(extra['shortness'])))#10*[6.4844e-01, 4.2873e-06, 8.9063e-01, 7.0317e-02, 5.4688e-01, 7.1737e-06],
+
             print('length_loss=:', self.stats['length_loss'].grad_fn)
 
         # Compute a cosine similarity loss between the (mean) summary representation that's fed to the
